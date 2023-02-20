959a04e59d6452eab7af9f59661e2e2943657508
José Augusto <joseaugusto.mello01@gmail.com>
2023-02-20 08:41:31 -0300

    FindAll para categoria

---
+++ src/main/java/com/io/github/AugustoMello09/bookstore/dto/CategoriaDTO.java
@@ -0,0 +1,50 @@
+package com.io.github.AugustoMello09.bookstore.dto;
+
+import com.io.github.AugustoMello09.bookstore.domain.Categoria;
+
+public class CategoriaDTO {
+
+	private Integer id;
+
+	private String nome;
+
+	private String descricao;
+
+	public CategoriaDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CategoriaDTO(Categoria obj) {
+		super();
+		this.id = obj.getId();
+		this.nome = obj.getNome();
+		this.descricao = obj.getDescricao();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	
+}
