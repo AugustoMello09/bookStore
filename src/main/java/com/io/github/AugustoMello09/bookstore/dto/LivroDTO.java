9d072abae6a623e5fe57a86d4d46350cba0f614c
José Augusto <joseaugusto.mello01@gmail.com>
2023-02-20 09:19:07 -0300

    GET para livro

---
+++ src/main/java/com/io/github/AugustoMello09/bookstore/dto/LivroDTO.java
@@ -0,0 +1,11 @@
+package com.io.github.AugustoMello09.bookstore.dto;
+
+public class LivroDTO {
+	/*private Integer id;
+	
+	private String titulo;
+	
+	private String nome_autor;
+	
+	private String texto;*/
+}
---
+++ src/main/java/com/io/github/AugustoMello09/bookstore/services/LivroService.java
@@ -0,0 +1,23 @@
+package com.io.github.AugustoMello09.bookstore.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.io.github.AugustoMello09.bookstore.domain.Livro;
+import com.io.github.AugustoMello09.bookstore.repositories.LivroRepository;
+import com.io.github.AugustoMello09.bookstore.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class LivroService {
+	
+	@Autowired
+	private LivroRepository repository;
+	
+	public Livro findById(Integer id) {
+		Optional<Livro> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado id:"
+				+ id + ", tipo: " + Livro.class.getName()));
+	}
+}
